b861bc383c3489e426e24efe0e5acb3ca8b89e41
rixjoseph <mca517@rajagiri.edu>
2021-05-31 21:13:33 +0530

    project complted9

---
+++ resources/views/signup.blade.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>STUDIO</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+
+</head>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">CRYSTAL</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Services</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About us</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#" tabindex="-1" aria-disabled="true">Contact us</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/signup" tabindex="-1" aria-disabled="true">SIGN UP</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/signin" tabindex="-1" aria-disabled="true">SIGN IN</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<body>
+
+<div class="container">
+<div class="row">
+<div class="col col-12">
+<center><h1>SIGN UP</h1></center> 
+<table class="table table-borderless">
+            <tr>
+                <td>Name</td>
+                <td><input type="text" class="form-control"></td>
+            </tr>
+            <tr>
+                <td>Course</td>
+                <td><input type="text" class="form-control"></td>
+            </tr>
+            <tr>
+                <td>Date Of Birth</td>
+                <td><input type="text" class="form-control"></td>
+            </tr>
+            <tr>
+                <td>Phone</td>
+                <td><input type="text" class="form-control"></td>
+            </tr>
+            <tr>
+                <td>Email</td>
+                <td><input type="text" class="form-control"></td>
+            </tr>
+            <tr>
+                <td>Password</td>
+                <td><input type="password" class="form-control"></td>
+            </tr>
+            <tr>
+                <td>Confrim Password</td>
+                <td><input type="Password" class="form-control"></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><button class="bt btn-outline-success">SIGN UP</button></td>
+            </tr>
+        </table>
+
+</div>
+</div>
+</div>
+
+
+ <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+   
+</body>
+</html>
